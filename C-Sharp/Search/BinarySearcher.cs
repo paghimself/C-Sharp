0265721fc0d6208283298526ed57c9b911feb25c
Philip Gelinas <philippeantoineg@canadel.ca>
2021-08-11 17:08:55 -0400

    Renamed C-Sharp original proj to Algorithms

---
+++ C-Sharp/Search/BinarySearcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Algorithms.Search
+{
+    public class BinarySearcher<T> where T : IComparable<T>
+    {
+        public int FindIndex(T[] sortedData, T item)
+        {
+            var leftIndex = 0;
+            var rightIndex = sortedData.Length;
+
+            while (leftIndex <= rightIndex)
+            {
+                var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+
+                if (item.CompareTo(sortedData[middleIndex]) > 0)
+                {
+                    leftIndex = middleIndex + 1;
+                    continue;
+                }
+
+                if (item.CompareTo(sortedData[middleIndex]) < 0)
+                {
+                    rightIndex = middleIndex - 1;
+                    continue;
+                }
+
+                return middleIndex;
+            }
+
+            return -1;
+        }
+    }
+}
