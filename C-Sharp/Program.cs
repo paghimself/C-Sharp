997ce220fb7adf72d7128adfd135d1be0550fb10
Philip Gelinas <philippeantoineg@canadel.ca>
2021-08-11 17:26:25 -0400

    The rename was successful

--- C-Sharp/Program.cs
+++ C-Sharp/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Algorithms.Search;
+using System;
 
-namespace C_Sharp
+namespace Algorithms
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bs = new BinarySearcher<int>();
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            Console.WriteLine(bs.FindIndex(arr, 2));
         }
     }
 }
